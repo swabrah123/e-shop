0b5a331bfd80544e598c50fdb20780bc5f7101cb
swab747-af <swab747@gmail.com>
2025-09-04 12:59:03 +0300

    new change

---
+++ server.php
@@ -0,0 +1,8 @@
+<?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Include your actual application entry point
+require __DIR__ . '/index.php';
